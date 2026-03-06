91beb89d2895a189101cb85d025dc2c7b216d18f
William01br <williamstcorreia123@gmail.com>
2026-03-06 09:39:18 -0300

    refactor: refactor connection with database

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { PrismaService } from './prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const prisma = new PrismaService();
-  await prisma.onModuleInit();
+  app.enableShutdownHooks();
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,6 +4,7 @@ import {
   OnModuleDestroy,
   Logger,
 } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from 'generated/prisma/client';
 
@@ -12,18 +13,22 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
-  constructor() {
-    const adapter = new PrismaPg({ url: process.env.DATABASE_URL });
-    super({ adapter });
+  constructor(private readonly configService: ConfigService) {
+    const databaseUrl = configService.get<string>('DATABASE_URL');
+
+    const adapter = new PrismaPg({ url: databaseUrl });
+
+    super({ adapter, log: ['warn', 'error'] });
   }
   private readonly logger = new Logger(PrismaService.name);
 
   async onModuleInit() {
     await this.$connect();
-    this.logger.log('Prisma connected...');
+    this.logger.log('Prisma connected');
   }
 
   async onModuleDestroy() {
     await this.$disconnect();
+    this.logger.log('Prisma disconnected');
   }
 }
